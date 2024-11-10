f02e424a5255d080ccbc58c8ba7835a06fe83309
Ihor Mikheyev <ihor.mikheiev@gmail.com>
2024-11-10 20:39:50 +0100

    added validation

--- src/db/models/Contacts.js
+++ src/db/models/Contacts.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import { CONTACT_TYPE_LIST } from '../../constants/contacts.js';
 
 const contactSchema = new Schema(
   {
@@ -19,7 +20,7 @@ const contactSchema = new Schema(
     },
     contactType: {
       type: String,
-      enum: ['work', 'home', 'personal'],
+      enum: CONTACT_TYPE_LIST,
       required: true,
       default: 'personal',
     },
